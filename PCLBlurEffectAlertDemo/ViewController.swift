78d531ce3042179bf8fe28b42dcb30557fdd6dba
NemSothe <nemsothea13@gmail.com>
2017-02-05 21:34:28 +0700

    PCLBLUREFFETALERT

--- PCLBlurEffectAlertDemo/ViewController.swift
+++ PCLBlurEffectAlertDemo/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import PCLBlurEffectAlert
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
+    
+    
+    @IBAction func CallAlert(_ sender: Any) {
+        let alert = PCLBlurEffectAlert.Controller(title: "PCLBlurEffectAlert Title", message: "PCLBlurEffectAlert Message", effect: UIBlurEffect(style: .dark), style: .actionSheet)
+        let alertbtn = PCLBlurEffectAlert.AlertAction(title: "Cancle", style: .cancel, handler: nil)
+       
+        
+        alert.addAction(alertbtn)
+        alert.configure(cornerRadius: 10)
+        alert.configure(overlayBackgroundColor: UIColor(red: 0, green: 60, blue: 5, alpha: 0.1))
+        alert.configure(titleFont: UIFont.boldSystemFont(ofSize: 15), titleColor: UIColor.lightGray)
+        alert.configure(messageFont: UIFont.systemFont(ofSize: 13), messageColor: UIColor.white)
+        alert.show()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
